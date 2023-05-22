c09b0c2d8c877163bbb3a63035aa201254266c01
ReemSaidd <reemahmed@aucegypt.edu>
2023-05-22 19:57:50 +0300

    GUI Separated

--- Alg.py
+++ Alg.py
@@ -1,5 +1,5 @@
 from Classes import Reservation_Station, Register_File
-from tkinter import Tk, Button, Entry, filedialog, Label
+from tkinter import Tk, Button, Entry, filedialog, Label, Text
 import tkinter as tk
 
 class Tomasulos_Algorithm:
@@ -439,73 +439,6 @@ def main():
 
 #main()
 
-class EducationalWindow(tk.Tk):
-    def __init__(self, reservation_stations, register_file, algorithm):
-        super().__init__()
-
-        self.reservation_stations = reservation_stations
-        self.register_file = register_file
-        self.algorithm = algorithm
-        self.title("Learn.")
-
-        self.grid_columnconfigure(0, weight=1)
-        self.grid_columnconfigure(1, weight=1)
-        self.grid_rowconfigure(9, weight=1)
-
-        self.station_labels = []
-        for i, station in enumerate(self.reservation_stations):
-            label_text = f"{station.Name}: Op={station.Op}, Vj={station.Vj}, Vk={station.Vk}, Qj={station.Qj}, Qk={station.Qk}, Busy={station.Busy}, A={station.A}"
-            label = tk.Label(self, text=label_text)
-            label.grid(row=i, column=0, padx=10, pady=5, sticky="w")
-            self.station_labels.append(label)
-
-        self.register_labels = []
-        for i in range(8):
-            label_text = f"R{i}: Value={self.register_file.getRegisterVal(f'R{i}')}, Qi={self.register_file.getRegisterQ(f'R{i}')}"
-            label = tk.Label(self, text=label_text)
-            label.grid(row=i, column=1, padx=10, pady=5, sticky="w")
-            self.register_labels.append(label)
-
-        self.ipc_label = tk.Label(self, text="IPC: ")
-        self.ipc_label.grid(row=len(self.reservation_stations) + 1, column=0, padx=10, pady=5, sticky="e")
-        self.prediction_label = tk.Label(self, text="Prediction: ")
-        self.prediction_label.grid(row=len(self.reservation_stations) + 1, column=1, padx=10, pady=5, sticky="w")
-
-        solid_line = tk.Frame(self, height=2, bd=1, relief=tk.SOLID)
-        solid_line.grid(row=len(self.reservation_stations) + 2, column=0, columnspan=2, padx=10, pady=5, sticky="we")
-
-        clock_label = tk.Label(self, text="Clock Cycle:")
-        clock_label.grid(row=len(self.reservation_stations) + 3, column=0, padx=10, pady=5, sticky="e")
-        self.clock_value_label = tk.Label(self, text="0")
-        self.clock_value_label.grid(row=len(self.reservation_stations) + 3, column=1, padx=10, pady=5, sticky="e")
-
-        start_button = tk.Button(self, text="Start Cycle", command=self.start_cycle)
-        start_button.grid(row=len(self.reservation_stations) + 4, column=0, columnspan=2, padx=10, pady=5)
-
-    def update_labels(self, clock_cycle):
-        for i, station in enumerate(self.reservation_stations):
-            label_text = f"{station.Name}: Op={station.Op}, Vj={station.Vj}, Vk={station.Vk}, Qj={station.Qj}, Qk={station.Qk}, Busy={station.Busy}, A={station.A}"
-            self.station_labels[i].config(text=label_text)
-
-        for i in range(8):
-            label_text = f"R{i}: Value={self.register_file.getRegisterVal(f'R{i}')}, Qi={self.register_file.getRegisterQ(f'R{i}')}"
-            self.register_labels[i].config(text=label_text)
-
-        self.clock_value_label.config(text=clock_cycle)
-
-    def start_cycle(self):
-        self.algorithm.startCycle()
-        self.update_labels(self.algorithm.Clock)
-        self.update_ipc_label()
-        self.update_prediction_label()
-
-    def update_ipc_label(self):
-        ipc = self.algorithm.getIPC()  # Replace this with the actual method to get IPC from your algorithm
-        self.ipc_label.config(text=f"IPC: {ipc}")
-
-    def update_prediction_label(self):
-        prediction = self.algorithm.getPrediction()  # Replace this with the actual method to get the prediction from your algorithm
-        self.prediction_label.config(text=f"Fail Percentage: {prediction}%")
 # algorithm = Tomasulos_Algorithm()
 # algorithm.readInstructionsFromFile("test.txt")
 # window = EducationalWindow(algorithm.Reservation_Stations, algorithm.registerFile, algorithm)
@@ -513,97 +446,6 @@ class EducationalWindow(tk.Tk):
 # window.mainloop()
 main()
 
-class StartWindow:
-    def __init__(self):
-        super().__init__()
-        self.root = Tk()
-        self.root.title("Tomasulos Algorithm")
-        self.root.geometry("400x400")
-        self.inputfile = ""
-        self.instructionLabel = ""
-        self.instructionEntry = Entry(self.root)
-        self.instructionEntry.pack()
-
-    def selectFile(self):
-        self.root = Tk()
-        self.root.withdraw()
-        self.inputfile = filedialog.askopenfilename()
-        if self.inputfile:
-            print("The selected file", self.inputfile)
-        else:
-            self.inputfile = ""
-            print("No file selected")
-        self.root.destroy()
-
-
-    def executeFile(self):
-        print(self.inputfile)
-        if self.inputfile:
-            algorithm = Tomasulos_Algorithm()
-            algorithm.readInstructionsFromFile(self.inputfile)
-            self.root = EducationalWindow(algorithm.Reservation_Stations, algorithm.registerFile, algorithm)
-            self.root.update_labels(algorithm.Clock)
-            self.root.mainloop()
-        else:
-            print("No file executing")
-
-    def submitInstructions(self):
-        SW = StartWindow() ##?
-        instructions = self.instructionEntry.get() ##The problem is instructionEntry is not defined
-        # instruction_lines = instructions.split('\n')
-        # for line in instruction_lines:
-        #     process_instruction(line)
-        self.saveInstructions(instructions)
-
-        print("Entered instructions: ", instructions)
-
-
-
-    def saveInstructions(self, instructions):
-        file_name = "instructions.txt"
-        with open(file_name, 'w') as file:
-            file.write(instructions)
-        print("Instructions saved to", file_name)
-
-        algorithm = Tomasulos_Algorithm()   ##
-        algorithm.readInstructionsFromFile(file_name)
-        EW = EducationalWindow(algorithm.Reservation_Stations, algorithm.registerFile, algorithm)
-
-    def executeWritten(self):
-        code_entry = Entry(self.submitInstructions())
-        code_entry.pack()
-
-        code  = code_entry.get()
-        print("Executing written code:")
-        print(code)
-
-        self.root.destroy()
-
-    def test(self):
-        fileSelectButton = Button(text = "Select the file", command =self.selectFile)
-        fileSelectButton.pack()
-
-        codeButton = Button(self.root, text = "Execute Written Code", command = self.executeWritten)
-        codeButton.pack()
-
-
-        fileButton = Button(self.root, text = "Execute File Code:", command =self.executeFile)
-        fileButton.pack()
-
-        submitButton = Button(self.root, text = "Submit", command = self.submitInstructions)
-        submitButton.pack()
-
-        #root = Tk()
-        instructionLabel = Label(self.root, text = "Enter Instructions: ")      #
-        instructionLabel.pack()     #
-
-        self.instructionEntry = Entry(self.root)     #
-        self.instructionEntry.pack()     #
-
-        self.root.mainloop()
-
-start = StartWindow()
-start.test()
 
 # algorithm = Tomasulos_Algorithm()
 # algorithm.readInstructionsFromFile("test.txt")
--- GUI.py
+++ GUI.py
@@ -1,67 +1,153 @@
-from Alg import Tomasulos_Algorithm as TA
-from tkinter import Tk, Button, Entry, filedialog,ttk
+from Alg import Tomasulos_Algorithm
+from tkinter import Tk, Button, Entry, filedialog, Label, Text
 import tkinter as tk
 
-class EducationalWindow(tk.Tk):
-    def __init__(self, root, TA):
-        table = ttk.Treeview(root)
-        self.TA = TA
-
-        # Define columns
-        table["columns"] = ("Name", "Busy", "Vj", "Vk", "Qj", "Qk", "A")  #add status later
-
-        # Format column headers
-        table.heading("Name", text="Name")
-        table.heading("Busy", text="Busy")
-        table.heading("Vj", text="Vj")
-        table.heading("Vk", text="Vk")
-        table.heading("Qj", text="Qj")
-        table.heading("Qk", text="Qk")
-        table.heading("A", text="A")
-
-        # Configure column widths
-        table.column("Name", width=100)  # Adjust the width as needed
-        table.column("Busy", width=50)
-        table.column("Vj", width=50)
-        table.column("Vk", width=50)
-        table.column("Qj", width=50)
-        table.column("Qk", width=50)
-        table.column("A", width=50)
-
-        # Add data rows
-        for i, station in enumerate(self.TA.Reservation_Stations):
-            table.insert("", "end", values=(station.Name, station.Busy, station.Vj, station.Vk, station.Qj, station.Qk, station.A))
-
-        # Display table
-        table.pack()
-
-class Initial_Screen(tk.Tk):
+class StartWindow:
     def __init__(self):
-        self.title = "Tomasulo's Algorithm"
-        self.geometry = "500x500"
+        super().__init__()
         self.root = Tk()
-
-        self.title_label = tk.Label(self, text="Enter instructions below")
-        self.title_label.pack()
-
-        self.instruction_entry = tk.Entry(self.root)
-        self.instruction_entry.pack()
-
-        self.instruction_button = tk.Button(self.root, text="Add Instruction", command=self.add_instruction)
-        self.instruction_button.pack()
+        self.root.title("Tomasulos Algorithm")
+        self.root.geometry("400x400")
+        self.inputfile = ""
+        self.instructionLabel = ""
+        self.instructionEntry = Label(self.root)
+        self.instructionEntry.pack()
 
 
-# Create an instance of the home screen
-# home_screen = Initial_Screen()
+    def selectFile(self):
+        self.root = Tk()
+        self.root.withdraw()
+        self.inputfile = filedialog.askopenfilename()
+        if self.inputfile:
+            print("The selected file", self.inputfile)
+        else:
+            self.inputfile = ""
+            print("No file selected")
+        self.root.destroy()
 
-# # Start the Tkinter event loop
-# home_screen.mainloop()
-# # Create root window
-# root = tk.Tk()
 
-# alg = TA()
-# # Call the function to create the table
-# ED = EducationalWindow( root, alg)
+    def executeFile(self):
+        print(self.inputfile)
+        if self.inputfile:
+            algorithm = Tomasulos_Algorithm()
+            algorithm.readInstructionsFromFile(self.inputfile)
+            self.root = EducationalWindow(algorithm.Reservation_Stations, algorithm.registerFile, algorithm)
+            self.root.update_labels(algorithm.Clock)
+            self.root.mainloop()
+        else:
+            print("No file executing")
 
-# # Start the Tkinter event loop
-# root.mainloop()
+    def submitInstructions(self):
+        SW = StartWindow() ##?
+        instructions = self.instructionEntry.get()
+        instructionsList = instructions.split(';')
+        self.saveInstructions(instructionsList)
+
+        print("Entered instructions: ", instructions)
+
+
+
+    def saveInstructions(self, instructionsList):
+        file_name = "instructions.txt"
+        instructions = "\n".join(instructionsList)
+        with open(file_name, 'w') as file:
+            file.write(instructions)
+        print("Instructions saved to", file_name)
+
+        algorithm = Tomasulos_Algorithm()
+        algorithm.readInstructionsFromFile(file_name)
+        EW = EducationalWindow(algorithm.Reservation_Stations, algorithm.registerFile, algorithm)
+
+    def test(self):
+        fileSelectButton = Button(text = "Select the file", command =self.selectFile)
+        fileSelectButton.pack()
+
+
+        fileButton = Button(self.root, text = "Execute File Code: ", command =self.executeFile)
+        fileButton.pack()
+
+        submitButton = Button(self.root, text = "Execute Written Code: ", command = self.submitInstructions)
+        submitButton.pack()
+
+        instructionLabel = Label(self.root, text = "Enter Instructions: ")
+        instructionLabel.pack()
+
+        self.instructionEntry = Entry(self.root)
+        self.instructionEntry.pack()
+
+        self.root.mainloop()
+
+
+class EducationalWindow(tk.Tk):
+    def __init__(self, reservation_stations, register_file, algorithm):
+        super().__init__()
+
+        self.reservation_stations = reservation_stations
+        self.register_file = register_file
+        self.algorithm = algorithm
+        self.title("Learn.")
+
+        self.grid_columnconfigure(0, weight=1)
+        self.grid_columnconfigure(1, weight=1)
+        self.grid_rowconfigure(9, weight=1)
+
+        self.station_labels = []
+        for i, station in enumerate(self.reservation_stations):
+            label_text = f"{station.Name}: Op={station.Op}, Vj={station.Vj}, Vk={station.Vk}, Qj={station.Qj}, Qk={station.Qk}, Busy={station.Busy}, A={station.A}"
+            label = tk.Label(self, text=label_text)
+            label.grid(row=i, column=0, padx=10, pady=5, sticky="w")
+            self.station_labels.append(label)
+
+        self.register_labels = []
+        for i in range(8):
+            label_text = f"R{i}: Value={self.register_file.getRegisterVal(f'R{i}')}, Qi={self.register_file.getRegisterQ(f'R{i}')}"
+            label = tk.Label(self, text=label_text)
+            label.grid(row=i, column=1, padx=10, pady=5, sticky="w")
+            self.register_labels.append(label)
+
+        self.ipc_label = tk.Label(self, text="IPC: ")
+        self.ipc_label.grid(row=len(self.reservation_stations) + 1, column=0, padx=10, pady=5, sticky="e")
+        self.prediction_label = tk.Label(self, text="Prediction: ")
+        self.prediction_label.grid(row=len(self.reservation_stations) + 1, column=1, padx=10, pady=5, sticky="w")
+
+        solid_line = tk.Frame(self, height=2, bd=1, relief=tk.SOLID)
+        solid_line.grid(row=len(self.reservation_stations) + 2, column=0, columnspan=2, padx=10, pady=5, sticky="we")
+
+        clock_label = tk.Label(self, text="Clock Cycle:")
+        clock_label.grid(row=len(self.reservation_stations) + 3, column=0, padx=10, pady=5, sticky="e")
+        self.clock_value_label = tk.Label(self, text="0")
+        self.clock_value_label.grid(row=len(self.reservation_stations) + 3, column=1, padx=10, pady=5, sticky="e")
+
+        start_button = tk.Button(self, text="Start Cycle", command=self.start_cycle)
+        start_button.grid(row=len(self.reservation_stations) + 4, column=0, columnspan=2, padx=10, pady=5)
+
+    def update_labels(self, clock_cycle):
+        for i, station in enumerate(self.reservation_stations):
+            label_text = f"{station.Name}: Op={station.Op}, Vj={station.Vj}, Vk={station.Vk}, Qj={station.Qj}, Qk={station.Qk}, Busy={station.Busy}, A={station.A}"
+            self.station_labels[i].config(text=label_text)
+
+        for i in range(8):
+            label_text = f"R{i}: Value={self.register_file.getRegisterVal(f'R{i}')}, Qi={self.register_file.getRegisterQ(f'R{i}')}"
+            self.register_labels[i].config(text=label_text)
+
+        self.clock_value_label.config(text=clock_cycle)
+
+    def start_cycle(self):
+        self.algorithm.startCycle()
+        self.update_labels(self.algorithm.Clock)
+        self.update_ipc_label()
+        self.update_prediction_label()
+
+    def update_ipc_label(self):
+        ipc = self.algorithm.getIPC()  # Replace this with the actual method to get IPC from your algorithm
+        self.ipc_label.config(text=f"IPC: {ipc}")
+
+    def update_prediction_label(self):
+        prediction = self.algorithm.getPrediction()  # Replace this with the actual method to get the prediction from your algorithm
+        self.prediction_label.config(text=f"Fail Percentage: {prediction}%")
+
+start = StartWindow()
+start.test()
+
+#take memory file
+#output memory in GUI
